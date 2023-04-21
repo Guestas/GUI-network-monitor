90cdf13f9572e244f5294394b35535c7fcc8fcf4
Martin Cé <72087021+Guestas@users.noreply.github.com>
2023-04-21 12:39:18 +0200

    ver 2.2.0

--- src/podpr/graphs.js
+++ src/podpr/graphs.js
@@ -9,7 +9,7 @@ window.api.receive("fromMainServerDown", (data) => {
 
 function loadAllDataNew(){
   const DaysAgo = new Date();
-  timestapm1dayago = DaysAgo.setDate(DaysAgo.getDate() - 1);
+  timestapm1dayago = DaysAgo.setDate(DaysAgo.getDate() - 3);
   console.log(timestapm1dayago, Date.now())
   dataForGraphsT(timestapm1dayago, undefined, "s", true, true)
 }
